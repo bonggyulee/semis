1ee440ac9a6539f49f1de847ddbc0b508fa4fa04
bonggyulee <45474714+bonggyulee@users.noreply.github.com>
2020-09-16 16:55:38 +0900

    Create start.bat

---
+++ window/start.bat
@@ -0,0 +1,29 @@
+cd \
+
+ 
+
+cd C:\Program Files (x86)\TOBESOFT\XPLATFORM\9.1
+start UXStudio.exe
+ 
+
+echo timeout 2 > nul or timeout /t 2
+timeout 2 > nul
+
+ 
+
+cd C:\Quest Software\Toad for Oracle 11.5
+start Toad.exe
+ 
+
+echo timeout 2 > nul or timeout /t 2
+timeout 2 > nul
+
+ 
+
+cd C:\eclipse_new\eclipse_201912
+start eclipse.exe
+
+
+rem start C:\Users\bong\Desktop\semis
+
+start C:\Users\bong\Desktop\semis\
